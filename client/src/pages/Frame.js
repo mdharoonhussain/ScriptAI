2206c85174d98a688e11ba27278682744bc1c8fe
Jayeshkhandelwal09 <khandelwaljayesh10@gmail.com>
2023-06-23 18:29:28 +0530

    Homepage

---
+++ client/src/pages/Frame.js
@@ -0,0 +1,103 @@
+import styles from "./Frame.module.css";
+const Frame = () => {
+  return (
+    <div className={styles.frame}>
+      <div className={styles.rectangle} />
+      <div className={styles.rectangle1} />
+      <div className={styles.frame1} />
+      <img className={styles.frameIcon} alt="" src="frames/frame.svg" />
+      <img className={styles.frameIcon1} alt="" src="frames/frame1.svg" />
+      <div className={styles.rectangle2} />
+      <div className={styles.rectangle3} />
+      <div className={styles.rectangle4} />
+      <div className={styles.rectangle5} />
+      <div className={styles.rectangle6} />
+      <div className={styles.rectangle7} />
+      <img className={styles.frameIcon2} alt="" src="frames/frame2.svg" />
+      <div className={styles.rectangle8} />
+      <div className={styles.rectangle9} />
+      <img className={styles.frameIcon3} alt="" src="frames/frame3.svg" />
+      <div className={styles.rectangle10} />
+      <div className={styles.rectangle11} />
+      <img className={styles.frameIcon4} alt="" src="frames/frame4.svg" />
+      <div className={styles.rectangle12} />
+      <img className={styles.frameIcon5} alt="" src="frames/frame5.svg" />
+      <img className={styles.frameIcon6} alt="" src="frames/frame6.svg" />
+      <div className={styles.rectangle13} />
+      <img className={styles.frameIcon7} alt="" src="frames/frame7.svg" />
+      <div className={styles.rectangle14} />
+      <div className={styles.rectangle14} />
+      <div className={styles.rectangle16} />
+      <img className={styles.frameIcon8} alt="" src="frames/frame8.svg" />
+      <img className={styles.imageIcon} alt="" src="frames/image.svg" />
+      <img className={styles.imageIcon1} alt="" src="frames/image1.svg" />
+      <img className={styles.imageIcon2} alt="" src="frames/image2.svg" />
+      <img className={styles.imageIcon3} alt="" src="frames/image3.svg" />
+      <div className={styles.scriptai}>ScriptAI</div>
+      <div className={styles.login}>Login</div>
+      <div className={styles.jobRoles}>Sign Up</div>
+      <div className={styles.logout}>Logout</div>
+      <div className={styles.unleashYourInterview}>
+        Unleash Your Interview Skills
+      </div>
+      <div className={styles.welcomeToScriptai}>
+        Welcome to ScriptAI Job Interview Prep, where we turn interview anxiety
+        into job offer excitement! Our AI-driven platform transforms the way you
+        prepare for interviews, providing personalized feedback and practice for
+        various roles.
+      </div>
+      <div className={styles.div}>500+</div>
+      <div className={styles.jobRoles1}>Job Roles</div>
+      <div className={styles.div1}>50+</div>
+      <div className={styles.interviewTypes}>Interview Types</div>
+      <div className={styles.k}>10K+</div>
+      <div className={styles.preparedUsers}>Prepared Users</div>
+      <div className={styles.getReady}>Get Ready</div>
+      <div className={styles.dontLeaveYour}>
+        Don't leave your dream job to chance. Get the AI-powered edge and tap
+        into the magic of ScriptAI Interview Preparation today!
+      </div>
+      <div className={styles.startPreparing}>Start Preparing</div>
+      <div className={styles.scriptaiHelpedMe}>
+        ScriptAI helped me land my dream job with ease!
+      </div>
+      <div className={styles.johnTurner}>John Turner</div>
+      <div className={styles.iNailedMy}>
+        I nailed my interview with ScriptAI’s custom feedback!
+      </div>
+      <div className={styles.mayaMurphy}>Maya Murphy</div>
+      <div className={styles.thisPlatformIs}>
+        This platform is fun, engaging and absolutely effective
+      </div>
+      <div className={styles.aliceJohnson}>Alice Johnson</div>
+      <div className={styles.howItWorks}>How It Works</div>
+      <div className={styles.selectFromA}>
+        Select from a diverse range of job roles and interview types, and let
+        ScriptAI’s advanced AI technology provide you with questions tailored to
+        your chosen path.
+      </div>
+      <div className={styles.giveYourBest}>
+        Give your best response in the text box provided and receive
+        personalized feedback on your performance instantly.
+      </div>
+      <div className={styles.testYourSkills}>
+        Test your skills, refine your technique, and step into your interview
+        feeling confident and prepared.
+      </div>
+      <div className={styles.explore}>Explore</div>
+      <div className={styles.home}>Home</div>
+      <div className={styles.caseStudies}>Case Studies</div>
+      <div className={styles.resource}>Resource</div>
+      <div className={styles.blog}>Blog</div>
+      <div className={styles.tips}>Tips</div>
+      <div className={styles.faqs}>FAQs</div>
+      <div className={styles.social}>Social</div>
+      <div className={styles.facebook}>Facebook</div>
+      <div className={styles.linkedin}>LinkedIn</div>
+      <div className={styles.twitter}>Twitter</div>
+      <div className={styles.scriptai2023}>© ScriptAI 2023</div>
+    </div>
+  );
+};
+
+export default Frame;
